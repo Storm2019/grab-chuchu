ff7460d8d423aec8f3e73cf8fa85575151a5a794
Chan <Christian.jayme@ddp.kyocera.com>
2023-06-09 17:58:09 +0800

    3rd update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,87 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput, ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      header: () => false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-white">
+      {/* Header */}
+      <View className=" flex-row p-3  items-center  space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#000000" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#000000" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-1">
+        <View className="flex-row items-center flex-1 space-x-2 bg-gray-200 p-2">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardTypes="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon size={30} color="#000000" />
+      </View>
+
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+
+        {/* Featured Rows */}
+        {/* Featured */}
+        <FeaturedRow
+          id="1"
+          title="Featured"
+          description="Paid placements from our partners"
+          featuredCategory="featured"
+        />
+        {/* Tasty discounts*/}
+        <FeaturedRow
+          id="2"
+          title="Tasty discounts"
+          description="Paid placements from our partners"
+          featuredCategory="featured"
+        />
+        {/* Offers near you */}
+        <FeaturedRow
+          id="3"
+          title="Offers near you"
+          description="Paid placements from our partners"
+          featuredCategory="featured"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
